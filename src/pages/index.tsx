594615f36568be5939bd74a9eeb8b19a71f4c91e
indiu6 <hewas6@gmail.com>
2022-01-24 09:30:43 -0500

    add random index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { getOptionsForVote } from '@/utils/getRandomPokemon';
 import { trpc } from '@/utils/trpc';
 import type { NextPage } from 'next';
 
@@ -6,14 +7,16 @@ const Home: NextPage = () => {
   // if (isLoading) return <div>Loading...</div>;
   // if (data) return <div>{data.greeting}</div>;
 
+  const [first, second] = getOptionsForVote();
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-16 h-16 bg-red-800">{first}</div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-16 h-16 bg-red-800">{second}</div>
       </div>
     </div>
   );
